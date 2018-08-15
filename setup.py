84280e5ef95971b1b99b8980aeba2ede55b1e213
Oliver Sauder <oliver.sauder@adfinis-sygroup.ch>
2018-08-15 11:01:37 +0200

    Adjust setup.py to caluma context

--- setup.py
+++ setup.py
@@ -5,9 +5,8 @@ from setuptools import find_packages, setup
 setup(
     name='caluma',
     version='0.0.0',
-    author='Adfinis SyGroup AG',
-    author_email='https://adfinis-sygroup.ch/',
     description='Caluma Service providing GraphQL API ',
-    url='https://adfinis-sygroup.ch/',
+    url='https://projectcaluma.github.io/',
+    license='MIT',
     packages=find_packages(),
 )
