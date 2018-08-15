0986b6a6986f54ec590e912e342e38913cb07662
Oliver Sauder <sliverc@users.noreply.github.com>
2018-08-15 16:13:11 +0200

    Replace django json api with graphene (#8)

--- caluma/urls.py
+++ caluma/urls.py
@@ -1,8 +1,4 @@
-from django.conf.urls import include, url
-from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
+from django.conf.urls import url
+from graphene_django.views import GraphQLView
 
-urlpatterns = [
-    url(r"^api-token-auth/", obtain_jwt_token),
-    url(r"^api-token-refresh/", refresh_jwt_token),
-    url(r"^api/v1/", include("caluma.form.urls")),
-]
+urlpatterns = [url(r"^graphql", GraphQLView.as_view(graphiql=True))]
