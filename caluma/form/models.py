b319ce6052f6f86c1d48b25d16abdf90f2790cec
Oliver Sauder <oliver.sauder@adfinis-sygroup.ch>
2018-08-23 16:40:29 +0200

    Allow description to be blank

--- caluma/form/models.py
+++ caluma/form/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Form(models.Model):
     slug = models.SlugField(max_length=50, primary_key=True)
     name = models.CharField(max_length=255)
-    description = models.TextField()
+    description = models.TextField(blank=True)
     meta = JSONField(default={})
     # TODO: ManyToMany questions
 
