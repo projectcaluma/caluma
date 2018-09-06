d1650ab0d6766c783f3fbdcc72a76ac5bcb1d6ac
Oliver Sauder <oliver.sauder@adfinis-sygroup.ch>
2018-09-06 10:35:06 +0200

    Handle empty values in LocalizedFilter
    This is needed in version django-filter 2.0.0

--- caluma/filters.py
+++ caluma/filters.py
@@ -1,9 +1,13 @@
 from django.utils import translation
+from django_filters.constants import EMPTY_VALUES
 from django_filters.rest_framework import Filter
 
 
 class LocalizedFilter(Filter):
     def filter(self, qs, value):
+        if value in EMPTY_VALUES:
+            return qs
+
         lang = translation.get_language()
         filter_expr = "{0}__{1}__{2}".format(self.field_name, lang, self.lookup_expr)
         return qs.filter(**{filter_expr: value})
