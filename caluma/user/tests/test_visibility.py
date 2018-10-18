22b60bfa56e706bd5cdc2104cfe48c2fce9c8e93
Oliver Sauder <oliver.sauder@adfinis-sygroup.ch>
2018-10-18 12:41:50 +0200

    Add visibility to only view nodes created by group user belogsn to

--- caluma/user/tests/test_visibility.py
+++ caluma/user/tests/test_visibility.py
@@ -3,7 +3,7 @@ import pytest
 from ...models import UUIDModel
 from ...tests.fake_model import get_fake_model
 from ...types import DjangoObjectType
-from ..visibilities import Authenticated
+from ..visibilities import Authenticated, CreatedByGroup
 
 
 @pytest.mark.parametrize("info_fixture,size", [("info", 0), ("admin_info", 1)])
@@ -18,4 +18,19 @@ def test_authenticated_visibility(db, info_fixture, size, request):
     FakeModel.objects.create()
 
     queryset = Authenticated().get_queryset(CustomNode, FakeModel.objects, info)
+    assert queryset.count() == size
+
+
+@pytest.mark.parametrize("group,size", [("unknown", 0), ("group", 1)])
+def test_created_by_group_visibility(db, admin_info, group, admin_user, size, request):
+    admin_user.groups = [group]
+    FakeModel = get_fake_model(model_base=UUIDModel)
+
+    class CustomNode(DjangoObjectType):
+        class Meta:
+            model = FakeModel
+
+    FakeModel.objects.create(created_by_group="group")
+
+    queryset = CreatedByGroup().get_queryset(CustomNode, FakeModel.objects, admin_info)
     assert queryset.count() == size
--- caluma/user/visibilities.py
+++ caluma/user/visibilities.py
@@ -8,3 +8,11 @@ class Authenticated(BaseVisibility):
         if info.context.user.is_authenticated:
             return queryset
         return queryset.none()
+
+
+class CreatedByGroup(BaseVisibility):
+    """User may only read nodes of created by group it belongs to."""
+
+    def get_base_queryset(self, node, queryset, info):
+        groups = info.context.user.groups
+        return queryset.filter(created_by_group__in=groups)
