54a3f0b226078a4f7a89cf2a15aa96bb55b5174b
Oliver Sauder <oliver.sauder@adfinis-sygroup.ch>
2018-12-11 10:20:05 +0100

    Add more extract_tasks test cases

--- caluma/workflow/tests/test_jexl.py
+++ caluma/workflow/tests/test_jexl.py
@@ -5,7 +5,14 @@ from ..jexl import FlowJexl
 
 @pytest.mark.parametrize(
     "expression,expected_tasks",
-    [("63 > 62 ? 'test'|task : ['test2', 'test3']|tasks", {"test", "test2", "test3"})],
+    [
+        (
+            "63 > 62 ? 'test'|task : ['test2', 'test3']|tasks",
+            {"test", "test2", "test3"},
+        ),
+        ("['test2', 'test3']|tasks", {"test2", "test3"}),
+        ("'test'|task", {"test"}),
+    ],
 )
 def test_flow_extract_tasks(expression, expected_tasks):
     jexl = FlowJexl()
