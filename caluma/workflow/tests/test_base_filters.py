0ce48cb868266c4abe1d8d352766c0c1619a7569
dnmx <open-dynaMIX@users.noreply.github.com>
2019-02-22 14:46:45 +0100

    Add filters for created_by_user, created_by_group (#299)
    Those fields are in the BaseModel, so the filters are available for all models.
    
    Closes #296

---
+++ caluma/workflow/tests/test_base_filters.py
@@ -0,0 +1,51 @@
+def test_base_model_created_by_user_filter(db, work_item_factory, schema_executor):
+    work_item_factory(created_by_user="Winston Smith")
+    work_item_factory(created_by_user="Martin")
+
+    query = """
+            query WorkItems($createdByUser: String!) {
+              allWorkItems(createdByUser: $createdByUser) {
+                edges {
+                  node {
+                    createdByUser
+                    createdByGroup
+                  }
+                }
+              }
+            }
+        """
+
+    result = schema_executor(query, variables={"createdByUser": "Winston Smith"})
+
+    assert not result.errors
+    assert len(result.data["allWorkItems"]["edges"]) == 1
+    assert (
+        result.data["allWorkItems"]["edges"][0]["node"]["createdByUser"]
+        == "Winston Smith"
+    )
+
+
+def test_base_model_created_by_group_filter(db, work_item_factory, schema_executor):
+    work_item_factory(created_by_group="Oceania")
+    work_item_factory(created_by_group="Eurasia")
+
+    query = """
+                query WorkItems($createdByGroup: String!) {
+                  allWorkItems(createdByGroup: $createdByGroup) {
+                    edges {
+                      node {
+                        createdByUser
+                        createdByGroup
+                      }
+                    }
+                  }
+                }
+            """
+
+    result = schema_executor(query, variables={"createdByGroup": "Oceania"})
+
+    assert not result.errors
+    assert len(result.data["allWorkItems"]["edges"]) == 1
+    assert (
+        result.data["allWorkItems"]["edges"][0]["node"]["createdByGroup"] == "Oceania"
+    )
