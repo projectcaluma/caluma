4761d8b44553e6aabc8dfa86fec0311277dec429
Fabio Rämi <fabio.raemi@adfinis-sygroup.ch>
2019-05-01 09:05:42 +0200

    Add removeAnswer mutation
    This commit adds a removeAnswer mutation.
    
    So if a user answered an optional question, there is now a way to completely
    remove it's answer.
    
    Closes #392

---
+++ caluma/form/tests/test_answer.py
@@ -0,0 +1,28 @@
+import pytest
+
+from .. import models
+
+
+@pytest.mark.parametrize(
+    "question__type,answer__value", [(models.Question.TYPE_INTEGER, 23)]
+)
+def test_remove_answer(db, snapshot, question, answer, schema_executor):
+    query = """
+        mutation RemoveAnswer($input: RemoveAnswerInput!) {
+          removeAnswer(input: $input) {
+            answer {
+              id
+              meta
+            }
+            clientMutationId
+          }
+        }
+    """
+
+    result = schema_executor(query, variables={"input": {"answer": str(answer.pk)}})
+
+    assert not result.errors
+    with pytest.raises(models.Answer.DoesNotExist):
+        answer.refresh_from_db()
+
+    snapshot.assert_match(result.data)
