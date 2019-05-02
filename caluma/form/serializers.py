projectcaluma/caluma

commit 5dd1361a68afd7294b4e406e9e42031611cdf224

fix(validation): add type mapping for validating file questions (#410)

caluma/form/serializers.py
@@ -681,11 +681,8 @@ class SaveDocumentFileAnswerSerializer(SaveAnswerSerializer):
         return super().create(validated_data)
 
     def update(self, instance, validated_data):
-        if instance.file and instance.file.name is not validated_data["file"]:
-            instance.file.delete()
-            validated_data = self.set_file(validated_data)
-        elif not instance.file:
-            validated_data = self.set_file(validated_data)
+        instance.file.delete()
+        validated_data = self.set_file(validated_data)
 
         return super().update(instance, validated_data)
 

caluma/form/factories.py
@@ -94,6 +94,13 @@ class DocumentFactory(DjangoModelFactory):
         model = models.Document
 
 
+class FileFactory(DjangoModelFactory):
+    name = Faker("file_name")
+
+    class Meta:
+        model = models.File
+
+
 class AnswerFactory(DjangoModelFactory):
     question = SubFactory(QuestionFactory)
     document = SubFactory(DocumentFactory)
@@ -107,6 +114,7 @@ class AnswerFactory(DjangoModelFactory):
         elif self.question.type not in [
             models.Question.TYPE_FORM,
             models.Question.TYPE_TABLE,
+            models.Question.TYPE_FILE,
         ]:
             return Faker("name").generate({})
 
@@ -115,12 +123,16 @@ class AnswerFactory(DjangoModelFactory):
     value_document = Maybe(
         "is_form", yes_declaration=SubFactory(DocumentFactory), no_declaration=None
     )
+    file = Maybe(
+        "is_file", yes_declaration=SubFactory(FileFactory), no_declaration=None
+    )
 
     class Meta:
         model = models.Answer
 
     class Params:
         is_form = LazyAttribute(lambda a: a.question.type == models.Question.TYPE_FORM)
+        is_file = LazyAttribute(lambda a: a.question.type == models.Question.TYPE_FILE)
 
 
 class AnswerDocumentFactory(DjangoModelFactory):
@@ -130,10 +142,3 @@ class AnswerDocumentFactory(DjangoModelFactory):
 
     class Meta:
         model = models.AnswerDocument
-
-
-class FileFactory(DjangoModelFactory):
-    name = Faker("file_name")
-
-    class Meta:
-        model = models.File

caluma/form/tests/test_validators.py
@@ -31,6 +31,17 @@ def test_validate_hidden_required_field(
 
 
 @pytest.mark.parametrize(
+    "question__type,question__is_required", [(Question.TYPE_FILE, "false")]
+)
+def test_validate_file_field(
+    db, form_question, question, document_factory, answer_factory
+):
+    document = document_factory(form=form_question.form)
+    answer_factory(document=document, question=question)
+    DocumentValidator().validate(document)
+
+
+@pytest.mark.parametrize(
     "required_jexl,should_throw",
     [
         ("true", True),

caluma/form/validators.py
@@ -140,6 +140,10 @@ class DocumentValidator:
                     get_document_answers(document)
                     for document in answer.documents.all()
                 ]
+
+            elif answer.question.type == Question.TYPE_FILE:
+                return answer.file.name
+
             else:  # pragma: no cover
                 raise Exception("unhandled question type mapping")
 

commit 3df3dcf2092bb695acca1748ff2e458581e07dd8

Do only recreate the File object if the file name changed
This commit fixes a regression from #410.

caluma/form/serializers.py
@@ -681,9 +681,9 @@ class SaveDocumentFileAnswerSerializer(SaveAnswerSerializer):
         return super().create(validated_data)
 
     def update(self, instance, validated_data):
-        instance.file.delete()
-        validated_data = self.set_file(validated_data)
-
+        if instance.file.name is not validated_data["file"]:
+            instance.file.delete()
+            validated_data = self.set_file(validated_data)
         return super().update(instance, validated_data)
 
     class Meta(SaveAnswerSerializer.Meta):