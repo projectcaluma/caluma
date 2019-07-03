aee2279a06d6e5a5b5c89e2a1868b6aab0b1df1d
David Vogt <david.vogt@adfinis-sygroup.ch>
2019-07-03 18:42:31 +0200

    feat(filters): Meta lookup: extend to LT,GT,LTE,GTE

--- caluma/tests/snapshots/snap_test_schema.py
+++ caluma/tests/snapshots/snap_test_schema.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 
 from snapshottest import Snapshot
 
+
 snapshots = Snapshot()
 
 snapshots[
@@ -660,6 +661,10 @@ enum MetaLookupMode {
   EXACT
   STARTSWITH
   CONTAINS
+  GTE
+  GT
+  LTE
+  LT
 }
 
 input MetaValueFilterType {
