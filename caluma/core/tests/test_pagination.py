b184264fe86a4e8df70221b5f1c05769c24c9f68
Oliver Sauder <os@esite.ch>
2019-07-08 09:00:22 +0200

    fix!: Remove erroneous offset and limit pagination filters
    BREaKING CHANGE use relay pagination instead

--- caluma/core/tests/test_pagination.py
+++ caluma/core/tests/test_pagination.py
@@ -1,36 +1,6 @@
 import pytest
 
 
-def test_offset_pagination(db, schema_executor, document_factory):
-    document_factory(meta={"position": 0})
-    document_factory(meta={"position": 1})
-    document_factory(meta={"position": 2})
-    document_factory(meta={"position": 3})
-    document_factory(meta={"position": 4})
-
-    query = """
-        query AllDocumentsQuery {
-          allDocuments(limit: 2, offset: 2) {
-            totalCount
-            edges {
-              node {
-                id
-                meta
-              }
-            }
-          }
-        }
-    """
-
-    result = schema_executor(query)
-
-    assert not result.errors
-    assert len(result.data["allDocuments"]["edges"]) == 2
-    assert result.data["allDocuments"]["totalCount"] == 2
-    assert result.data["allDocuments"]["edges"][0]["node"]["meta"]["position"] == 2
-    assert result.data["allDocuments"]["edges"][1]["node"]["meta"]["position"] == 3
-
-
 @pytest.mark.parametrize(
     "first,last,before,after,has_next,has_previous",
     [
