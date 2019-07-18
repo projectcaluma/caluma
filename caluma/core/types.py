7b9bffdf2b22aca5d6c2d260abeaf3be28c4569a
Oliver Sauder <os@esite.ch>
2019-07-18 16:53:00 +0200

    Integrate upstream manager check in DjangoConnectionField
    https://github.com/graphql-python/graphene-django/pull/693

--- caluma/core/types.py
+++ caluma/core/types.py
@@ -73,7 +73,7 @@ class DjangoConnectionField(DjangoConnectionField):
             iterable = default_manager
         iterable = maybe_queryset(iterable)
         if isinstance(iterable, QuerySet):
-            if iterable is not default_manager:
+            if iterable.model.objects is not default_manager:
                 default_queryset = maybe_queryset(default_manager)
                 iterable = cls.merge_querysets(default_queryset, iterable)
 
