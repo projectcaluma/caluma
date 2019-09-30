a66582a4945c3562828cdca284e3fff8da11af98
Fabio Rämi <fabio.ambauen@adfinis-sygroup.ch>
2019-09-30 14:08:06 +0200

    feat(history): add cleanup command
    This commit adds a cleanup command for historical records:
    
    ```
    usage: manage.py cleanup_history [-h] [--dry] [-k KEEP] [--version]
                                     [-v {0,1,2,3}] [--settings SETTINGS]
                                     [--pythonpath PYTHONPATH] [--traceback]
                                     [--no-color] [--force-color]
    
    Cleanup historical records.
    
    optional arguments:
      -h, --help            show this help message and exit
      --dry
      -k KEEP, --keep KEEP  Duration we want to keep the records. E.g. '6 months',
                            '1 year'. Uses dateparser.
    ```

---
+++ caluma/core/management/commands/cleanup_history.py
@@ -0,0 +1,42 @@
+from dateparser import parse
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+from simple_history.models import registered_models
+
+
+class Command(BaseCommand):
+    """Cleanup historical records."""
+
+    help = "Cleanup historical records."
+
+    def add_arguments(self, parser):
+        parser.add_argument("--dry", dest="dry", action="store_true")
+        parser.add_argument(
+            "-k",
+            "--keep",
+            dest="keep",
+            default="",
+            help=(
+                "Duration we want to keep the records. "
+                "E.g. '6 months', '1 year'. Uses dateparser."
+            ),
+        )
+
+    def handle(self, *args, **options):
+        lt = parse(options["keep"])
+        if lt is not None:
+            lt = timezone.make_aware(lt)
+
+        for _, model in registered_models.items():
+            qs = model.history.all()
+            if lt is not None:
+                qs = model.history.filter(history_date__lt=lt)
+
+            action_str = "Would delete"
+            if not options["dry"]:
+                action_str = "Deleting"
+            self.stdout.write(
+                f'{action_str} {qs.count()} historical records from model "{model.__name__}"'
+            )
+            if not options["dry"]:
+                qs.delete()
