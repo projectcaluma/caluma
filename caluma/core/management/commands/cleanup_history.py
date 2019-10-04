9191b4e3638f866d256ac3b40a59b6839a6d4ef5
Stefan Borer <stefan.borer@adfinis-sygroup.ch>
2019-10-04 13:05:01 +0200

    fix(history): cleanup_history - add force argument

--- caluma/core/management/commands/cleanup_history.py
+++ caluma/core/management/commands/cleanup_history.py
@@ -10,12 +10,12 @@ class Command(BaseCommand):
     help = "Cleanup historical records."
 
     def add_arguments(self, parser):
-        parser.add_argument("--dry", dest="dry", action="store_true")
+        parser.add_argument("--force", dest="force", default=False, action="store_true")
         parser.add_argument(
             "-k",
             "--keep",
             dest="keep",
-            default="",
+            default="1 year",
             help=(
                 "Duration we want to keep the records. "
                 "E.g. '6 months', '1 year'. Uses dateparser."
@@ -23,6 +23,8 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
+        force = options["force"]
+
         lt = parse(options["keep"])
         if lt is not None:
             lt = timezone.make_aware(lt)
@@ -32,11 +34,9 @@ class Command(BaseCommand):
             if lt is not None:
                 qs = model.history.filter(history_date__lt=lt)
 
-            action_str = "Would delete"
-            if not options["dry"]:
-                action_str = "Deleting"
+            action_str = "Deleting" if force else "Would delete"
             self.stdout.write(
                 f'{action_str} {qs.count()} historical records from model "{model.__name__}"'
             )
-            if not options["dry"]:
+            if force:
                 qs.delete()
