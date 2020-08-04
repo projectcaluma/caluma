6754f0538512109e0dc2bc78709786d9a5f7c9a2
Jonas Metzener <jonas.metzener@adfinis.com>
2020-08-04 11:21:02 +0200

    chore: add docs for extension class and improve groups transform

--- caluma/caluma_workflow/dynamic_groups.py
+++ caluma/caluma_workflow/dynamic_groups.py
@@ -2,6 +2,8 @@ from functools import wraps
 
 
 def register_dynamic_group(dynamic_group_name):
+    """Decorate function to register a dynamic group."""
+
     def wrapper(fn, *arg, **kwarg):
         fn._dynamic_group_name = dynamic_group_name
 
@@ -15,6 +17,28 @@ def register_dynamic_group(dynamic_group_name):
 
 
 class BaseDynamicGroups:
+    """Basic dynamic groups class to be extended by any dynamic groups class implementation.
+
+    In combination with the decorator `@register_dynamic_group` a dynamic group
+    class can extend the JEXL `groups` transform used in the `address_groups`
+    and `control_groups` property of a task.
+
+    A dynamic groups class could look like this:
+
+    >>> from your.project.code import find_legal_dept
+    ... from caluma.caluma_workflow.dynamic_groups import (
+    ...     BaseDynamicGroups,
+    ...     register_dynamic_group,
+    ... )
+    ...
+    ...
+    ... class CustomDynamicGroups(BaseDynamicGroups):
+    ...     @register_dynamic_group("legal_dept")
+    ...     def resolve_legal_dept(self, task, case, user, prev_work_item, context):
+    ...         # custom business logic which returns either a string or a list of strings
+    ...         return find_legal_dept(case)
+    """
+
     def resolve(self, dynamic_group_name):
         for methodname in dir(self):
             method = getattr(self, methodname)
