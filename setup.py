92cdc7ff3531f6be093456b67ea8607b01c046ad
Stefan Borer <stefan.borer@adfinis.com>
2020-08-04 15:14:10 +0200

    chore(deps): update graphene-django
    Update to graphene-django 2.12.1 and fix connection field, schema.

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ setup(
         "django-watchman<2",
         "djangorestframework<4",
         "django_simple_history<4",
-        "graphene-django<=2.8.2",
+        "graphene-django>=3.0.0",
         "idna<4",
         "minio >= 7, < 8",
         "psycopg2-binary >= 2.8, <2.9",
