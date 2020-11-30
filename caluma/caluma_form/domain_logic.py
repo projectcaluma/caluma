projectcaluma/caluma

commit 6b71559eb7a1dca280de625c89d7b4e65c02decc

fix: use domain_logic for saving answers in form app

caluma/caluma_form/domain_logic.py
@@ -26,13 +26,13 @@ class SaveAnswerLogic:
     ) -> dict:
         question = data["question"]
 
-        if question.type == models.Question.TYPE_TABLE:
+        if question.type == models.Question.TYPE_TABLE and not data.get("documents"):
             data["documents"] = models.Document.objects.filter(pk__in=data["value"])
             del data["value"]
-        elif question.type == models.Question.TYPE_FILE:
+        elif question.type == models.Question.TYPE_FILE and not data.get("file"):
             data["file"] = data["value"]
             del data["value"]
-        elif question.type == models.Question.TYPE_DATE:
+        elif question.type == models.Question.TYPE_DATE and not data.get("date"):
             data["date"] = data["value"]
             del data["value"]
 
@@ -86,6 +86,9 @@ class SaveAnswerLogic:
 
         if answer.question.type == models.Question.TYPE_TABLE:
             answer.create_answer_documents(documents)
+
+        answer.refresh_from_db()
+        return answer
 
     @staticmethod
     def set_file(validated_data):

caluma/caluma_form/tests/__snapshots__/test_document.ambr
@@ -361,7 +361,7 @@
   }
 ---
 # name: test_save_document_answer[choice-question__configuration17-None-question__format_validators17-option-slug-None-SaveDocumentStringAnswer-True-option-slug-False-True]
-  None
+  Answer(document=Document(form=Form(slug=suggest-traditional)), question=Question(slug=effort-meet, type=choice), value='option-slug')
 ---
 # name: test_save_document_answer[choice-question__configuration17-None-question__format_validators17-option-slug-None-SaveDocumentStringAnswer-True-option-slug-True-False]
   <class 'OrderedDict'> {
@@ -389,7 +389,7 @@
   }
 ---
 # name: test_save_document_answer[date-question__configuration7-None-question__format_validators7-None-2019-02-22-SaveDocumentDateAnswer-True-option-slug-False-True]
-  None
+  Answer(document=Document(form=Form(slug=suggest-traditional)), question=Question(slug=effort-meet, type=date), value=None)
 ---
 # name: test_save_document_answer[date-question__configuration7-None-question__format_validators7-None-2019-02-22-SaveDocumentDateAnswer-True-option-slug-True-False]
   <class 'OrderedDict'> {
@@ -417,7 +417,7 @@
   }
 ---
 # name: test_save_document_answer[dynamic_choice-question__configuration21-MyDataSource-question__format_validators21-5.5-None-SaveDocumentStringAnswer-True-option-slug-False-True]
-  None
+  Answer(document=Document(form=Form(slug=suggest-traditional)), question=Question(slug=effort-meet, type=dynamic_choice), value='5.5')
 ---
 # name: test_save_document_answer[dynamic_choice-question__configuration21-MyDataSource-question__format_validators21-5.5-None-SaveDocumentStringAnswer-True-option-slug-True-False]
   <class 'OrderedDict'> {
@@ -448,7 +448,7 @@
   }
 ---
 # name: test_save_document_answer[dynamic_multiple_choice-question__configuration19-MyDataSource-question__format_validators19-answer__value19-None-SaveDocumentListAnswer-True-option-slug-False-True]
-  None
+  Answer(document=Document(form=Form(slug=suggest-traditional)), question=Question(slug=effort-meet, type=dynamic_multiple_choice), value=['5.5', '1'])
 ---
 # name: test_save_document_answer[dynamic_multiple_choice-question__configuration19-MyDataSource-question__format_validators19-answer__value19-None-SaveDocumentListAnswer-True-option-slug-True-False]
   <class 'OrderedDict'> {
@@ -482,7 +482,7 @@
   }
 ---
 # name: test_save_document_answer[file-question__configuration10-None-question__format_validators10-not-exist.pdf-None-SaveDocumentFileAnswer-True-option-slug-False-True]
-  None
+  Answer(document=Document(form=Form(slug=suggest-traditional)), question=Question(slug=effort-meet, type=file), value=None)
 ---
 # name: test_save_document_answer[file-question__configuration10-None-question__format_validators10-not-exist.pdf-None-SaveDocumentFileAnswer-True-option-slug-True-False]
   <class 'OrderedDict'> {
@@ -516,7 +516,7 @@
   }
 ---
 # name: test_save_document_answer[file-question__configuration9-None-question__format_validators9-some-file.pdf-None-SaveDocumentFileAnswer-True-option-slug-False-True]
-  None
+  Answer(document=Document(form=Form(slug=suggest-traditional)), question=Question(slug=effort-meet, type=file), value=None)
 ---
 # name: test_save_document_answer[file-question__configuration9-None-question__format_validators9-some-file.pdf-None-SaveDocumentFileAnswer-True-option-slug-True-False]
   <class 'OrderedDict'> {
@@ -547,7 +547,7 @@
   }
 ---
 # name: test_save_document_answer[float-question__configuration2-None-question__format_validators2-2.1-None-SaveDocumentFloatAnswer-True-option-slug-False-True]
-  None
+  Answer(document=Document(form=Form(slug=suggest-traditional)), question=Question(slug=effort-meet, type=float), value=2.1)
 ---
 # name: test_save_document_answer[float-question__configuration2-None-question__format_validators2-2.1-None-SaveDocumentFloatAnswer-True-option-slug-True-False]
   <class 'OrderedDict'> {
@@ -575,7 +575,7 @@
   }
 ---
 # name: test_save_document_answer[integer-question__configuration0-None-question__format_validators0-1-None-SaveDocumentIntegerAnswer-True-option-slug-False-True]
-  None
+  Answer(document=Document(form=Form(slug=suggest-traditional)), question=Question(slug=effort-meet, type=integer), value=1)
 ---
 # name: test_save_document_answer[integer-question__configuration0-None-question__format_validators0-1-None-SaveDocumentIntegerAnswer-True-option-slug-True-False]
   <class 'OrderedDict'> {
@@ -605,7 +605,7 @@
   }
 ---
 # name: test_save_document_answer[multiple_choice-question__configuration15-None-question__format_validators15-answer__value15-None-SaveDocumentListAnswer-True-option-slug-False-True]
-  None
+  Answer(document=Document(form=Form(slug=suggest-traditional)), question=Question(slug=effort-meet, type=multiple_choice), value=['option-slug'])
 ---
 # name: test_save_document_answer[multiple_choice-question__configuration15-None-question__format_validators15-answer__value15-None-SaveDocumentListAnswer-True-option-slug-True-False]
   <class 'OrderedDict'> {
@@ -646,7 +646,7 @@
   }
 ---
 # name: test_save_document_answer[table-question__configuration12-None-question__format_validators12-None-None-SaveDocumentTableAnswer-True-option-slug-False-True]
-  None
+  Answer(document=Document(form=Form(slug=treatment-radio)), question=Question(slug=effort-meet, type=table), value=None)
 ---
 # name: test_save_document_answer[table-question__configuration12-None-question__format_validators12-None-None-SaveDocumentTableAnswer-True-option-slug-True-False]
   <class 'OrderedDict'> {
@@ -685,7 +685,7 @@
   }
 ---
 # name: test_save_document_answer[text-question__configuration24-None-question__format_validators24-test@example.com-None-SaveDocumentStringAnswer-True-option-slug-False-True]
-  None
+  Answer(document=Document(form=Form(slug=suggest-traditional)), question=Question(slug=effort-meet, type=text), value='test@example.com')
 ---
 # name: test_save_document_answer[text-question__configuration24-None-question__format_validators24-test@example.com-None-SaveDocumentStringAnswer-True-option-slug-True-False]
   <class 'OrderedDict'> {
@@ -713,7 +713,7 @@
   }
 ---
 # name: test_save_document_answer[text-question__configuration4-None-question__format_validators4-Test-None-SaveDocumentStringAnswer-True-option-slug-False-True]
-  None
+  Answer(document=Document(form=Form(slug=suggest-traditional)), question=Question(slug=effort-meet, type=text), value='Test')
 ---
 # name: test_save_document_answer[text-question__configuration4-None-question__format_validators4-Test-None-SaveDocumentStringAnswer-True-option-slug-True-False]
   <class 'OrderedDict'> {
@@ -741,7 +741,7 @@
   }
 ---
 # name: test_save_document_answer[textarea-question__configuration13-None-question__format_validators13-Test-None-SaveDocumentStringAnswer-True-option-slug-False-True]
-  None
+  Answer(document=Document(form=Form(slug=suggest-traditional)), question=Question(slug=effort-meet, type=textarea), value='Test')
 ---
 # name: test_save_document_answer[textarea-question__configuration13-None-question__format_validators13-Test-None-SaveDocumentStringAnswer-True-option-slug-True-False]
   <class 'OrderedDict'> {
@@ -769,7 +769,7 @@
   }
 ---
 # name: test_save_document_answer[textarea-question__configuration26-None-question__format_validators26-test@example.com-None-SaveDocumentStringAnswer-True-option-slug-False-True]
-  None
+  Answer(document=Document(form=Form(slug=suggest-traditional)), question=Question(slug=effort-meet, type=textarea), value='test@example.com')
 ---
 # name: test_save_document_answer[textarea-question__configuration26-None-question__format_validators26-test@example.com-None-SaveDocumentStringAnswer-True-option-slug-True-False]
   <class 'OrderedDict'> {

caluma/caluma_form/serializers.py
@@ -475,15 +475,19 @@ class DocumentSerializer(serializers.ModelSerializer):
 
 class SaveAnswerSerializer(serializers.ModelSerializer):
     def validate(self, data):
-        if "value" not in data:
-            data["value"] = None
-        validators.AnswerValidator().validate(
-            **data,
-            user=self.context["request"].user,
-            instance=self.instance,
-            origin=True,
+        return domain_logic.SaveAnswerLogic.validate_for_save(
+            data, self.context["request"].user, self.instance, True
         )
-        return super().validate(data)
+
+    @transaction.atomic
+    def create(self, validated_data):
+        return domain_logic.SaveAnswerLogic.create(
+            validated_data, self.context["request"].user
+        )
+
+    @transaction.atomic
+    def update(self, instance, validated_data):
+        return domain_logic.SaveAnswerLogic.update(validated_data, instance)
 
     class Meta:
         model = models.Answer
@@ -534,24 +538,6 @@ class SaveDocumentTableAnswerSerializer(SaveAnswerSerializer):
         help_text="List of document IDs representing the rows in the table.",
     )
 
-    @transaction.atomic
-    def create(self, validated_data):
-        documents = validated_data.pop("documents")
-        instance = super().create(validated_data)
-        instance.create_answer_documents(documents)
-        return instance
-
-    @transaction.atomic
-    def update(self, instance, validated_data):
-        documents = validated_data.pop("documents")
-
-        instance.unlink_unused_rows(docs_to_keep=documents)
-        instance.refresh_from_db()
-
-        instance = super().update(instance, validated_data)
-        instance.create_answer_documents(documents)
-        return instance
-
     class Meta(SaveAnswerSerializer.Meta):
         pass
 
@@ -559,18 +545,6 @@ class SaveDocumentTableAnswerSerializer(SaveAnswerSerializer):
 class SaveDocumentFileAnswerSerializer(SaveAnswerSerializer):
     value = CharField(write_only=True, source="file", required=False)
     value_id = PrimaryKeyRelatedField(read_only=True, source="file", required=False)
-
-    @transaction.atomic
-    def create(self, validated_data):
-        validated_data = domain_logic.SaveAnswerLogic.set_file(validated_data)
-        return super().create(validated_data)
-
-    @transaction.atomic
-    def update(self, instance, validated_data):
-        if instance.file.name is not validated_data["file"]:
-            instance.file.delete()
-            validated_data = domain_logic.SaveAnswerLogic.set_file(validated_data)
-        return super().update(instance, validated_data)
 
     class Meta(SaveAnswerSerializer.Meta):
         fields = SaveAnswerSerializer.Meta.fields + ("value_id",)

commit ded47955f4aeeca08b1e2a2965079d41b2229e60

fix(form): fix empty answers
This fixes a regression introduced in 6b71559eb7a1dca280de625c89d7b4e65c02decc

caluma/caluma_form/domain_logic.py
@@ -41,6 +41,11 @@ class SaveAnswerLogic:
     ) -> dict:
         question = data["question"]
 
+        # Save empty answer by omitting the value property since graphql-core
+        # doesn't allow `null` literals yet
+        if "value" not in data:
+            data["value"] = None
+
         if question.type == models.Question.TYPE_TABLE and not data.get("documents"):
             data["documents"] = models.Document.objects.filter(pk__in=data["value"])
             del data["value"]

caluma/caluma_form/tests/__snapshots__/test_document.ambr
@@ -785,7 +785,7 @@
 # name: test_save_document_answer[textarea-question__configuration26-None-question__format_validators26-test@example.com-None-SaveDocumentStringAnswer-True-option-slug-True-True]
   Answer(document=Document(form=Form(slug=suggest-traditional)), question=Question(slug=effort-meet, type=textarea), value='test@example.com')
 ---
-# name: test_save_document_answer_empty[text-false-None-False]
+# name: test_save_document_answer_empty[text-false-old value that needs to be deleted-False]
   <class 'OrderedDict'> {
     'saveDocumentStringAnswer': <class 'dict'> {
       'answer': <class 'dict'> {
@@ -796,7 +796,7 @@
     },
   }
 ---
-# name: test_save_document_answer_empty[text-false-None-True]
+# name: test_save_document_answer_empty[text-false-old value that needs to be deleted-True]
   <class 'OrderedDict'> {
     'saveDocumentStringAnswer': <class 'dict'> {
       'answer': <class 'dict'> {

caluma/caluma_form/tests/test_document.py
@@ -843,7 +843,7 @@ def test_save_document_answer(
 @pytest.mark.parametrize("delete_answer", [True, False])
 @pytest.mark.parametrize(
     "question__type,question__is_required,answer__value",
-    [(Question.TYPE_TEXT, "false", None)],
+    [(Question.TYPE_TEXT, "false", "old value that needs to be deleted")],
 )
 def test_save_document_answer_empty(
     db, snapshot, question, answer, schema_executor, delete_answer