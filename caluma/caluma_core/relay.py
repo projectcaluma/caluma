966fa445a19d304ac0fa048accac36341aedd4c3
David Vogt <david.vogt@adfinis.com>
2021-02-24 09:50:24 +0100

    fix: extract_global_id should not be too eager in decoding
    We need to validate the extracted type in `extract_global_id()`, as it
    may lead to errors when passing a valid slug, that's also valid base64,
    and happens to contain a colon.
    
    Instead of naively decoding and returning an invalid (type, identifier)
    pair, validate the type's name and behave correctly in those cases.

--- caluma/caluma_core/relay.py
+++ caluma/caluma_core/relay.py
@@ -1,5 +1,7 @@
 from graphql_relay import from_global_id
 
+_valid_types = set()
+
 
 def extract_global_id(id):
     """
@@ -8,9 +10,32 @@ def extract_global_id(id):
     In case it is not a base64 encoded value it will return id as is.
     This way it can be used as input type by simply defining id without its specific type.
     """
+    if not _valid_types:
+        # Lazy import due to circular dependency
+        from caluma import schema
+        from caluma.caluma_core import types
+
+        all_types_dict = {
+            **vars(schema.workflow_schema),
+            **vars(schema.form_schema),
+            **vars(schema.form_historical_schema),
+        }
+        all_types = [
+            typename
+            for typename, gql_type in all_types_dict.items()
+            if isinstance(gql_type, type) and issubclass(gql_type, types.Node)
+        ]
+
+        _valid_types.update(all_types)
 
     try:
-        _, result = from_global_id(id)
+        gql_type, result = from_global_id(id)
+
+        # some valid ids could decode to a string that from_global_id()
+        # assumes is a global id, so we need to verify that it is indeed
+        # the case.
+        if gql_type and gql_type not in _valid_types:
+            return id
     except ValueError:
         result = id
 
