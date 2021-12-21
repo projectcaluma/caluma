90d8e4d371dbb4b0f568232e8c398d7586fa70c9
David Vogt <david.vogt@adfinis.com>
2021-12-21 17:29:18 +0100

    fix(schema): make schema tests work again (sort of)
    The schema test relies on stringification of the root schema object.
    This currently fails due to a function within the graphql core package
    being unable to handle django lazy translation objects.
    
    The test setup is rigged such that once upstream fixes the problem, it
    will trigger a failure here and we can remove the hacky workaround.

--- caluma/tests/test_schema.py
+++ caluma/tests/test_schema.py
@@ -35,5 +35,76 @@ def test_schema_node(db, snapshot, request, node_type):
     assert result.data["node"]["id"] == global_id
 
 
-def test_schema_introspect(snapshot):
+# The following is a strict copy of the function
+# `graphql.language.block_string.print_block_string` with the issue
+# fixed that it cannot handle lazy strings coming from django translations.
+# TODO: Remove once patched upstream
+def _print_block_string(
+    value: str, indentation: str = "", prefer_multiple_lines: bool = False
+) -> str:
+    """Print a block string in the indented block form.
+
+    Prints a block string in the indented block form by adding a leading and
+    trailing blank line. However, if a block string starts with whitespace and
+    is a single-line, adding a leading blank line would strip that whitespace.
+
+    For internal use only.
+    """
+    is_single_line = "\n" not in value
+    has_leading_space = value.startswith(" ") or value.startswith("\t")
+    has_trailing_quote = value.endswith('"')
+    has_trailing_slash = value.endswith("\\")
+    print_as_multiple_lines = (
+        not is_single_line
+        or has_trailing_quote
+        or has_trailing_slash
+        or prefer_multiple_lines
+    )
+
+    # Format a multi-line block quote to account for leading space.
+    if print_as_multiple_lines and not (is_single_line and has_leading_space):
+        result = "\n" + indentation
+    else:
+        result = ""
+
+    # str(value) is required, as value might also be a lazy object
+    result += value.replace("\n", "\n" + indentation) if indentation else str(value)
+    if print_as_multiple_lines:
+        result += "\n"
+
+    return '"""' + result.replace('"""', '\\"""') + '"""'
+
+
+def test_schema_introspect_direct(snapshot, mocker):
+    import graphql.language.block_string
+
+    # FIXME: UGLY WORKAROUND
+    # This patches the print_block_string function within graphql core,
+    # as it's currently having problems with lazy-translated objects
+    # (output of ugettext_lazy(), for example).
+    # Once this is fixed, we can remove the patched version above and
+    # use our own.
+    #
+    # This is only relevant to the tests - GraphQL frontends query
+    # the schema via GraphQL, which isn't subject to the bug above
+
+    old_code = graphql.language.block_string.print_block_string.__code__
+    graphql.language.block_string.print_block_string.__code__ = (
+        _print_block_string.__code__
+    )
+
+    try:
+        snapshot.assert_match(str(schema))
+    finally:
+        graphql.language.block_string.print_block_string.__code__ = old_code
+
+
+@pytest.mark.xfail(strict=True)
+def test_schema_introspect_direct_unpatched(snapshot):
+    # This is the old test for checking the schema.
+    # It is currently expected to fail, as graphql has a bug preventing
+    # it from working.
+    # Once this start XFAILing, remove the above patchy version and
+    # remove the xfail flag here
+
     snapshot.assert_match(str(schema))
