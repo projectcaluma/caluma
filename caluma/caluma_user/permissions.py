6e1945b7a75f15b9ffefcb33173d4202523147af
Jonas Metzener <jonas.metzener@adfinis.com>
2022-08-26 14:07:45 +0200

    fix(permissions): make sure user permission classes call super
    Fixes #1830

--- caluma/caluma_user/permissions.py
+++ caluma/caluma_user/permissions.py
@@ -5,11 +5,16 @@ class IsAuthenticated(BasePermission):
     """Only allow authenticated users to execute mutations."""
 
     def has_permission(self, mutation, info):
-        return info.context.user.is_authenticated
+        return info.context.user.is_authenticated and super().has_permission(
+            mutation, info
+        )
 
 
 class CreatedByGroup(BasePermission):
     """Only allow mutating data that belongs to same group as current user."""
 
     def has_object_permission(self, mutation, info, instance):
-        return instance.created_by_group in info.context.user.groups
+        return (
+            instance.created_by_group in info.context.user.groups
+            and super().has_object_permission(mutation, info, instance)
+        )
