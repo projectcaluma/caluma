projectcaluma/caluma

commit 59eadbc799a2486f7606ce6d5d1eac3d12b86398

feat(analytics): enable reordering of analytics fields

caluma/caluma_analytics/migrations/0005_analytics_field_ordering.py
@@ -1,0 +1,27 @@
+# Generated by Django 3.2.15 on 2022-10-24 11:10
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("caluma_analytics", "0004_simple_history"),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name="analyticsfield",
+            options={"ordering": ["sort", "alias"]},
+        ),
+        migrations.AddField(
+            model_name="analyticsfield",
+            name="sort",
+            field=models.PositiveIntegerField(db_index=True, default=0, editable=False),
+        ),
+        migrations.AddField(
+            model_name="historicalanalyticsfield",
+            name="sort",
+            field=models.PositiveIntegerField(db_index=True, default=0, editable=False),
+        ),
+    ]

caluma/caluma_analytics/models.py
@@ -91,11 +91,13 @@ class AnalyticsField(UUIDModel):
     )
 
     show_output = models.BooleanField(default=True)
+    sort = models.PositiveIntegerField(editable=False, db_index=True, default=0)
 
     def __repr__(self):
         return f"AnalyticsField<{self.table.slug}.{self.alias}>"
 
     class Meta:
+        ordering = ["sort", "alias"]
         constraints = [
             UniqueConstraint(
                 name="unique_data_source",

caluma/caluma_analytics/tests/__snapshots__/test_simple_table.ambr
@@ -93,8 +93,14 @@
                 'edges': list([
                   dict({
                     'node': dict({
-                      'alias': 'statuuuuuus',
-                      'value': 'running',
+                      'alias': 'created_at',
+                      'value': '2021-10-10 00:00:00+00:00',
+                    }),
+                  }),
+                  dict({
+                    'node': dict({
+                      'alias': 'quarter',
+                      'value': '4',
                     }),
                   }),
                   dict({
@@ -105,14 +111,8 @@
                   }),
                   dict({
                     'node': dict({
-                      'alias': 'created_at',
-                      'value': '2021-10-10 00:00:00+00:00',
-                    }),
-                  }),
-                  dict({
-                    'node': dict({
-                      'alias': 'quarter',
-                      'value': '4',
+                      'alias': 'statuuuuuus',
+                      'value': 'running',
                     }),
                   }),
                 ]),

caluma/caluma_analytics/tests/__snapshots__/test_table.ambr
@@ -56,14 +56,14 @@
             'edges': list([
               dict({
                 'node': dict({
-                  'alias': 'quarter',
-                  'dataSource': 'created_at.quarter',
+                  'alias': 'existing',
+                  'dataSource': 'meta.foobar',
                 }),
               }),
               dict({
                 'node': dict({
-                  'alias': 'existing',
-                  'dataSource': 'meta.foobar',
+                  'alias': 'quarter',
+                  'dataSource': 'created_at.quarter',
                 }),
               }),
             ]),
@@ -89,14 +89,14 @@
             'edges': list([
               dict({
                 'node': dict({
-                  'alias': 'foobar',
-                  'dataSource': 'meta.baz',
+                  'alias': 'existing',
+                  'dataSource': 'meta.foobar',
                 }),
               }),
               dict({
                 'node': dict({
-                  'alias': 'existing',
-                  'dataSource': 'meta.foobar',
+                  'alias': 'foobar',
+                  'dataSource': 'meta.baz',
                 }),
               }),
             ]),

caluma/caluma_analytics/pivot_table.py
@@ -84,6 +84,12 @@ class PivotTable:
 
         return sql_query, params
 
+    @cached_property
+    def field_ordering(self):
+        return list(
+            self.table.fields.all().order_by("sort").values_list("alias", flat=True)
+        )
+
     def get_records(self):
         self._summary = defaultdict(int)
         sql_query, params = self.get_sql_and_params()

caluma/caluma_analytics/schema.py
@@ -78,8 +78,8 @@ class AnalyticsOutput(ObjectType):
         rows = [
             AnalyticsRow(
                 edges=[
-                    {"node": {"alias": alias, "value": val}}
-                    for alias, val in row.items()
+                    {"node": {"alias": alias, "value": row[alias]}}
+                    for alias in table.field_ordering
                 ]
             )
             for row in table.get_records()
@@ -91,8 +91,8 @@ class AnalyticsOutput(ObjectType):
         summary_row = table.get_summary()
         return AnalyticsRow(
             edges=[
-                {"node": {"alias": alias, "value": val}}
-                for alias, val in summary_row.items()
+                {"node": {"alias": alias, "value": summary_row[alias]}}
+                for alias in table.field_ordering
             ]
         )
 
@@ -169,7 +169,7 @@ class AnalyticsField(DjangoObjectType):
 
     @classmethod
     def get_queryset(cls, queryset, info):
-        return super().get_queryset(queryset, info).order_by("-created_at")
+        return super().get_queryset(queryset, info)
 
     class Meta:
         model = models.AnalyticsField
@@ -204,12 +204,20 @@ class RemoveAnalyticsField(Mutation):
         model_operations = ["update"]
 
 
+class ReorderAnalyticsFields(Mutation):
+    class Meta:
+        serializer_class = serializers.ReorderAnalyticsFieldSerializer
+        lookup_input_kwarg = "table"
+        model_operations = ["update"]
+
+
 class Mutation:
     save_analytics_table = SaveAnalyticsTable().Field()
     remove_analytics_table = RemoveAnalyticsTable().Field()
 
     save_analytics_field = SaveAnalyticsField().Field()
     remove_analytics_field = RemoveAnalyticsField().Field()
+    reorder_analytics_fields = ReorderAnalyticsFields().Field()
 
 
 class Query:

caluma/caluma_analytics/serializers.py
@@ -120,3 +120,29 @@ class RemoveAnalyticsFieldSerializer(serializers.ModelSerializer):
     class Meta:
         fields = ["id"]
         model = models.AnalyticsField
+
+
+class TableFieldRelatedField(serializers.GlobalIDPrimaryKeyRelatedField):
+    def get_queryset(self):
+        table = self.parent.parent.instance
+        return table.fields.all()
+
+
+class ReorderAnalyticsFieldSerializer(serializers.ModelSerializer):
+    table = serializers.GlobalIDField(source="slug")
+    fields = TableFieldRelatedField(many=True)
+
+    def update(self, instance, validated_data):
+        for sort, analytic_field in enumerate(validated_data["fields"], start=1):
+            models.AnalyticsField.objects.filter(
+                table=instance, pk=analytic_field.pk
+            ).update(sort=sort)
+
+        return instance
+
+    class Meta:
+        model = models.AnalyticsTable
+        fields = [
+            "table",
+            "fields",
+        ]

caluma/caluma_analytics/simple_table.py
@@ -1028,6 +1028,12 @@ class SimpleTable:
             )
         return fields
 
+    @cached_property
+    def field_ordering(self):
+        return list(
+            self.table.fields.all().order_by("sort").values_list("alias", flat=True)
+        )
+
     def get_sql_and_params(self):
         """Return a list of records as specified in the given table config."""
 

commit 34be9bfedfd893e0dd86f117df8b61d011359f53

fix: fix breaking change introduced in 59eadbc

caluma/caluma_analytics/migrations/0005_analytics_field_ordering.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
     operations = [
         migrations.AlterModelOptions(
             name="analyticsfield",
-            options={"ordering": ["sort", "alias"]},
+            options={"ordering": ["sort", "-created_at"]},
         ),
         migrations.AddField(
             model_name="analyticsfield",

caluma/caluma_analytics/models.py
@@ -97,7 +97,7 @@ class AnalyticsField(UUIDModel):
         return f"AnalyticsField<{self.table.slug}.{self.alias}>"
 
     class Meta:
-        ordering = ["sort", "alias"]
+        ordering = ["sort", "-created_at"]
         constraints = [
             UniqueConstraint(
                 name="unique_data_source",

caluma/caluma_analytics/tests/__snapshots__/test_simple_table.ambr
@@ -99,14 +99,14 @@
                   }),
                   dict({
                     'node': dict({
-                      'alias': 'foo',
-                      'value': 'bar',
+                      'alias': 'quarter',
+                      'value': '4',
                     }),
                   }),
                   dict({
                     'node': dict({
-                      'alias': 'quarter',
-                      'value': '4',
+                      'alias': 'foo',
+                      'value': 'bar',
                     }),
                   }),
                   dict({

caluma/caluma_analytics/tests/__snapshots__/test_table.ambr
@@ -56,14 +56,14 @@
             'edges': list([
               dict({
                 'node': dict({
-                  'alias': 'existing',
-                  'dataSource': 'meta.foobar',
+                  'alias': 'quarter',
+                  'dataSource': 'created_at.quarter',
                 }),
               }),
               dict({
                 'node': dict({
-                  'alias': 'quarter',
-                  'dataSource': 'created_at.quarter',
+                  'alias': 'existing',
+                  'dataSource': 'meta.foobar',
                 }),
               }),
             ]),
@@ -89,14 +89,14 @@
             'edges': list([
               dict({
                 'node': dict({
-                  'alias': 'existing',
-                  'dataSource': 'meta.foobar',
+                  'alias': 'foobar',
+                  'dataSource': 'meta.baz',
                 }),
               }),
               dict({
                 'node': dict({
-                  'alias': 'foobar',
-                  'dataSource': 'meta.baz',
+                  'alias': 'existing',
+                  'dataSource': 'meta.foobar',
                 }),
               }),
             ]),