5913fe5e461e616ce882737671683df4c0b02631
Fabio Ambauen <fabio@dnmx.ch>
2023-02-17 15:20:22 +0100

    fix: replace faulty migration for calc answers with a command
    The migration `0047_recalculate_calc_answers` was error prone and took a
    long time to execute. This commit replaces it with a management command
    that is more optimized and stable. Still this operation will take some
    time to finish.

---
+++ caluma/caluma_form/management/commands/recalculate_calc_answers.py
@@ -0,0 +1,35 @@
+from django.core.management.base import BaseCommand
+
+from caluma.caluma_form.models import Answer, Question
+from caluma.caluma_form.signals import _update_or_create_calc_answer
+
+
+class Command(BaseCommand):
+    """
+    Recalculate calculated answers containing values from TableAnswers.
+
+    Due to a bug, answers to calculated questions were wrong, if they contained values
+    from table rows. This command recalculates all of them.
+    """
+
+    help = "Recalculate calculated answers containing values from TableAnswers."
+
+    def handle(self, *args, **options):
+        affected_questions = (
+            Question.objects.filter(type="table")
+            .exclude(calc_dependents=[])
+            .values_list("calc_dependents", flat=True)
+        )
+
+        affected_questions_clean = set(
+            [slug for entry in affected_questions for slug in entry]
+        )
+
+        calc_answers = Answer.objects.filter(
+            question__type="calculated_float",
+            document__isnull=False,
+            question__slug__in=affected_questions_clean,
+        )
+
+        for answer in calc_answers.iterator():
+            _update_or_create_calc_answer(answer.question, answer.document)
